57de90b25b64bd9b6e77cd46a4e4f2dc87aa75ba
Seonaid Lee <seonaidl@gmail.com>
2018-10-21 12:54:09 -0300

    Validation of inputs

--- iterate-map.rb
+++ iterate-map.rb
@@ -1,14 +1,49 @@
-puts "enter initial value for x"
-x_old = gets.chop.to_f
-puts "x(0) = #{x_old}"
+def make_fixed(number)
+   var = number.to_f
+   return var
+end
 
-puts "enter initial value for R"
-r_value = gets.chop.to_f
-puts "R = #{r_value}"
+def make_int(number)
+   var = number.to_i
+   return var
+end
 
-puts "How many iterations?"
-x_max = gets.chop.to_i
-puts "Will iterate = #{x_max} times"
+x0_defined = false
+R0_defined = false
+xmax_defined = false
+
+while not(x0_defined)
+  puts "Enter initial value for x"
+  x_old = make_fixed(gets.chop)
+  if x_old > 0 and x_old < 1 
+    puts "x(0) = #{x_old}"
+    x0_defined = true
+  else
+    puts "Initial value must be between 0 and 1"
+  end
+end
+
+while not(R0_defined)
+  puts "Enter initial value for R"
+  r_value = make_fixed(gets.chop)
+  if r_value > 0 and r_value < 4 
+    puts "R = #{r_value}"
+    R0_defined = true
+  else
+    puts "Initial value must be between 0 and 4"
+  end
+end
+
+while not(xmax_defined)
+  puts "How many iterations?"
+  x_max = make_int(gets.chop)
+  if x_max > 0
+    puts "Will iterate = #{x_max} times"
+    xmax_defined = true
+  else
+    puts "Must be a number greater than 0"
+  end
+end
 
 n=1
 while n <= x_max do
@@ -17,3 +52,4 @@ while n <= x_max do
   x_old = x_new
   n += 1
 end
+
