1fa1829003695000c96f04c2ce736697a19f075f
Seonaid Lee <seonaidl@gmail.com>
2018-10-21 11:20:08 -0300

    Initial commit gathering inputs, no validation

---
+++ iterate-map.rb
@@ -0,0 +1,11 @@
+puts "enter initial value for x"
+x_0 = gets.chop
+puts "x(0) = #{x_0}"
+
+puts "enter initial value for R"
+r_value = gets.chop
+puts "R = #{r_value}"
+
+puts "How many iterations?"
+x_max = gets.chop
+puts "Will iterate = #{x_max} times"
