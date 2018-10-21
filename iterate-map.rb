71b6164e283c9c6fa4c1b9797f9de12cfea43779
Seonaid Lee <seonaidl@gmail.com>
2018-10-21 11:41:45 -0300

    Implements logistic map loop. No validation of input. Not DRY.

--- iterate-map.rb
+++ iterate-map.rb
@@ -1,11 +1,19 @@
 puts "enter initial value for x"
-x_0 = gets.chop
-puts "x(0) = #{x_0}"
+x_old = gets.chop.to_f
+puts "x(0) = #{x_old}"
 
 puts "enter initial value for R"
-r_value = gets.chop
+r_value = gets.chop.to_f
 puts "R = #{r_value}"
 
 puts "How many iterations?"
-x_max = gets.chop
+x_max = gets.chop.to_i
 puts "Will iterate = #{x_max} times"
+
+n=1
+while n <= x_max do
+  x_new = r_value*x_old*(1-x_old)
+  puts "x_n = #{x_new}"
+  x_old = x_new
+  n += 1
+end
